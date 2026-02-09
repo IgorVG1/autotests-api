d0b30ee199dabdb248d316c829d4adc0c55963bd
IgorVG1 <IgorVG1@users.noreply.github.com>
2026-02-09 15:23:57 +0000

    deploy: 3be2cdb85379a487981a7547d6668cf89f9c488f

--- test.py
+++
@@ -1,7 +0,0 @@
-from faker import Faker
-
-fake = Faker(locale='ru_RU')
-
-print(f'fake name: {fake.name()}')
-print(f'fake address: {fake.address()}')
-print(f'fake email: {fake.email()}')
--- tools/allure/epics.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-
-class AllureEpic(str, Enum):
-
-    LMS = "LMS"
-    STUDENT = "Student service"
-    ADMINISTRATION = "Administration service"
